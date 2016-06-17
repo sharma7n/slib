42fa038b161d793fb2820f7eadaac786e2c2056f
sharma7n <sharma7n@gmail.com>
2016-06-16 21:33:08 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,16 +1,19 @@
-# from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
 
 from slib.models.holding import Holding, Item
 
 class HoldingList(ListView):
-	model = Holding
+    model = Holding
 	
 class HoldingDetail(DetailView):
-	model = Holding
+    model = Holding
 	
 class ItemList(ListView):
-	model = Item
+    model = Item
 	
 class ItemDetail(DetailView):
-	model = Item
+    model = Item
+    
+def index(request):
+    return render(request, 'slib/index.html')
